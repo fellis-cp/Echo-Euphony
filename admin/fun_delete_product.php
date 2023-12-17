7442b7013a57c6c88a19e4c29e6f90629e7aec9a
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2023-12-17 22:06:36 +0700

    rm image

---
+++ admin/fun_delete_product.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include 'admin_db.php';
+
+if(isset($_POST['submit3'])){
+    $id = $_POST['id'];
+    
+    // fetch image file
+    $sql = "SELECT gambar_produk FROM products WHERE id = $id";
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $imageToDelete = $row['p_image'];
+
+    // Delete dari db
+    $deleteQuery = "DELETE FROM products WHERE id = $id";
+    if(mysqli_query($con, $deleteQuery)){
+        // Delete the image file only if it's not being used by other products
+        $checkImageQuery = "SELECT COUNT(*) AS total FROM products WHERE gambar_produk = '$imageToDelete'";
+        $checkResult = mysqli_query($con, $checkImageQuery);
+        $row = mysqli_fetch_assoc($checkResult);
+        $totalCount = $row['total'];
+        
+        // jika img tidak di gunakan di produk lain hapus
+        if($totalCount == 0){
+            unlink($imageToDelete);
+        }
+        echo "<script>alert('Product berhasil di delete')</script>";
+        header('location: admin_home.php');
+        exit();
+    } else {
+        echo "<script>alert('Gagal menghapus product')</script>";
+    }
+}
+?>
